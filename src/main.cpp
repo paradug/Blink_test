8e423c9c5875b6e031855e2dc3daaf383ce15f6a
paradug <paradug@gmail.com>
2022-07-15 20:03:58 -0500

    Added serial messages

--- src/main.cpp
+++ src/main.cpp
@@ -25,12 +25,15 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("ON");
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("OFF");
   delay(1000);                       // wait for a second
 }
